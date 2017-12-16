258aad76b8db34e92878cae838b3b75f861c86dc
geosakel77 <mai1428@uom.edu.gr>
2017-12-16 10:05:34 +0100

    KClient V2 begin

---
+++ KClientV2.h
@@ -0,0 +1,67 @@
+//
+// Created by george on 16/11/2017.
+//
+
+#ifndef KClientV2_KClientV2_H
+#define KClientV2_KClientV2_H
+
+#include <iostream>
+#include <fstream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "FHE-SI.h"
+#include "Serialization.h"
+#include "clientfhesiutils.h"
+#include "unistd.h"
+#include <map>
+
+using namespace std;
+
+class KClientV2 {
+
+private:
+    string u_serverIP;
+    int u_serverPort;
+    int u_serverSocket=-1;
+    string t_serverIP;
+    int t_serverPort;
+    int t_serverSocket=-1;
+    bool verbose;
+
+    unsigned client_p;
+    unsigned client_g;
+    unsigned client_logQ;
+    unsigned dim; // dimension of the data
+    FHEcontext *client_context;
+    FHESISecKey *fhesiSecKey;
+    FHESIPubKey *fhesiPubKey;
+    KeySwitchSI *keySwitchSI;
+    FHESISecKey *fhesiSecKeyT;
+    KeySwitchSI *keySwitchSIT;
+    ifstream pkCToStream();
+    ifstream ksCToStream();
+    ifstream ksTToStream();
+    ifstream skTToStream();
+    ifstream contextToStream();
+    ifstream encryptedDataToStream(const Ciphertext &);
+    vector<ZZ_pX> loadeddata;
+    vector<ZZ_p> labels;
+    map<uint32_t ,ZZ_pX > encrypted_data_hash_table;
+    map<uint32_t ,unsigned> results;
+    void connectToUServer();
+    void connectToTServer();
+
+public:
+    KClientV2(unsigned, unsigned, unsigned,const string &,const string&,unsigned ,const string &, unsigned,bool verbose=true);
+    bool sendMessage(string, int socket);
+    bool sendStream(ifstream, int);
+    string receiveMessage(const int &,int buffersize=64);
+    void log(int,string);
+    void sendEncryptionParamToTServer();
+    void sendEncryptionParamToUServer();
+    void sendEncryptedDataToUServer();
+    void receiveResult();
+};
+
+
+#endif //KClientV2_KClientV2_H
--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 //
 
 //#include "KClient.h"
-#include "KClientV1.h"
+//#include "KClientV1.h"
+#include "KClientV2.h"
 #include <ctime>
 #include <chrono>
 #include <iomanip>
@@ -14,7 +15,9 @@ int main(){
     unsigned p=23;
     unsigned g=7;
     unsigned logQ=117;
-    KClientV1 client(p,g,logQ,"../sample.dat","127.0.0.1",5001,"127.0.0.1",5002,true);
+    //KClientV1 client(p,g,logQ,"../sample.dat","127.0.0.1",5001,"127.0.0.1",5002,true);
+    KClientV2 client(p,g,logQ,"../sample.dat","127.0.0.1",5001,"127.0.0.1",5002,true);
+
     //client.sendEncryptedData();
     //client.sendPublicKey("127.0.0.1",5001);
     //client.conn("127.0.0.1",5001);
